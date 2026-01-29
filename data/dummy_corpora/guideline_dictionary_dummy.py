86f8c3139dbc7c0b16e2a49bf9a0e97ad8f56209
ivancapobianco <83674565+ivancapobianco@users.noreply.github.com>
2026-01-29 16:06:23 +0100

    Update guideline_dictionary_dummy.py

--- data/dummy_corpora/guideline_dictionary_dummy.py
+++ data/dummy_corpora/guideline_dictionary_dummy.py
@@ -2,14 +2,14 @@
 
 guidelines_dict = {
     'esophageal': {
-        'path': "guidelines/NAME OF YOUR PDF.pdf",
+        'path': "guidelines/NAME OF YOUR PDF.pdf", # Make sure the PDF is located in the guidelines folder, and specify its path in the dictionary in the format guidelines/file_name.pdf.
         'starting_page': 21,
         'final_cleaned_chunk': "### 15 Tabellenverzeichnis",
         'mark': "### ",
         'images_to_save': [22, 29, 48],
     },
     'gastric': {
-        'path': "guidelines/NAME OF YOUR PDF.pdfF",
+        'path': "guidelines/NAME OF YOUR PDF.pdfF", # Make sure the PDF is located in the guidelines folder, and specify its path in the dictionary in the format guidelines/file_name.pdf.
         'starting_page': 28,
         'final_cleaned_chunk': "### 20 Tabellenverzeichnis",
         'mark': "### ",
@@ -19,14 +19,14 @@ guidelines_dict = {
 
 guidelines_nccn_dict = {
     'esophageal': {
-        'path': "guidelines/NAME OF YOUR PDF.pdf",
+        'path': "guidelines/NAME OF YOUR PDF.pdf", # Make sure the PDF is located in the guidelines folder, and specify its path in the dictionary in the format guidelines/file_name.pdf.
         'starting_page': 11,
         'final_page': 142,
         'mark': "### ",
         'images_to_save': [10, 11, 12, 13],
     },
     'gastric': {
-        'path': "guidelines/NAME OF YOUR PDF.pdf",
+        'path': "guidelines/NAME OF YOUR PDF.pdf", # Make sure the PDF is located in the guidelines folder, and specify its path in the dictionary in the format guidelines/file_name.pdf.
         'starting_page': 10,
         'final_page': 116,
         'mark': "### ",

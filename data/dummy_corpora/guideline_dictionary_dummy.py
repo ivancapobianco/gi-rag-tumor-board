36e228340ef61d18955b116574d6936f1a138fa6
ivancapobianco <83674565+ivancapobianco@users.noreply.github.com>
2026-01-29 11:57:36 +0100

    Create guideline_dictionary_dummy.py

---
+++ data/dummy_corpora/guideline_dictionary_dummy.py
@@ -0,0 +1,35 @@
+# Minimal dummy dictionary for S3 and NCCN guidelines
+
+guidelines_dict = {
+    'OE-CA': {
+        'path': "PATH OF YOUR PDF",
+        'starting_page': 21,
+        'final_cleaned_chunk': "### 15 Tabellenverzeichnis",
+        'mark': "### ",
+        'images_to_save': [22, 29, 48],
+    },
+    'Magen-CA': {
+        'path': "PATH OF YOUR PDF",
+        'starting_page': 28,
+        'final_cleaned_chunk': "### 20 Tabellenverzeichnis",
+        'mark': "### ",
+        'images_to_save': [41],
+    },
+}
+
+guidelines_nccn_dict = {
+    'OE-CA': {
+        'path': "PATH OF YOUR PDF",
+        'starting_page': 11,
+        'final_page': 142,
+        'mark': "### ",
+        'images_to_save': [10, 11, 12, 13],
+    },
+    'Magen-CA': {
+        'path': "PATH OF YOUR PDF",
+        'starting_page': 10,
+        'final_page': 116,
+        'mark': "### ",
+        'images_to_save': [9, 11, 13],
+    },
+}
